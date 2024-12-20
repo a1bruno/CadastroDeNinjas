7859a48282e5da071d9601c49350346fb7b6bd60
bruno <brunotrdev@gmail.com>
2024-12-20 10:33:25 -0300

    feat: Added the Lombok annotation to both Models: Ninja and Missoes

--- src/main/java/dev/java10x/aluno/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/aluno/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,51 +2,35 @@ package dev.java10x.aluno.CadastroDeNinjas.Missoes;
 
 import dev.java10x.aluno.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.LinkedList;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String dificuldade;
+
     @OneToMany(mappedBy = "missao")
     private LinkedList<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
+    @Override
+    public String toString() {
+        return "MissoesModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", dificuldade='" + dificuldade + '\'' +
+                ", ninjas=" + ninjas +
+                '}';
     }
 }
