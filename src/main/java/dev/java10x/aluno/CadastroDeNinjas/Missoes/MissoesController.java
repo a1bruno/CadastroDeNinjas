0b48f79d59861046e846d7b09152b6f8d25c6a90
bruno <brunotrdev@gmail.com>
2025-01-04 12:16:57 -0300

    feat: creates routes for 'MissoesController' using mock endpoints

--- src/main/java/dev/java10x/aluno/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/aluno/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,16 +1,28 @@
 package dev.java10x.aluno.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/missoes")
 public class MissoesController {
 
-    @GetMapping("/missoes")
+    @GetMapping("/show")
     public String abaMissoes() {
         return "Essa é a aba de missões";
     }
+
+    @PostMapping("/criar-missao")
+    public String criarMissao() { return "missao criada com sucesso!"; }
+
+    @GetMapping("/mostrar-missao_id")
+    public String mostrarMissao() { return "mostrando missao"; }
+
+    @PutMapping("/atualizar-missao_id")
+    public String atualizarMissao() { return "missao atualizada com sucesso!"; }
+
+    @DeleteMapping("/deleter-missao_id")
+    public String deletarMissao() { return "missao deletada com sucesso"; }
+
+
 
 }
